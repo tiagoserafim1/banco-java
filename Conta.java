ce12eb28918a860dee6d55cb7b500ca7078de1cb
Tiago Serafim de Souza <tiagosouza336@outlook.com>
2026-03-01 19:24:53 -0300

    Add files via upload

---
+++ Conta.java
@@ -0,0 +1,32 @@
+class Conta {
+    String nome;
+    double saldo;
+    int numeroConta;
+
+    public Conta(String nome, double saldo, int numeroConta) {
+        this.nome = nome;
+        this.saldo = saldo;
+        this.numeroConta = numeroConta;
+    }
+
+    boolean depositar(double valor) {
+        if (valor <= 0) {
+            return false;
+        } else {
+            this.saldo = this.saldo + valor;
+            return true;
+        }
+    }
+
+    boolean sacar(double valor2) {
+        if (this.saldo >= valor2){
+            this.saldo = this.saldo - valor2;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    double verSaldo() {
+        return this.saldo;
+    }
+}
---
+++ Teste11.java
@@ -0,0 +1,6 @@
+public class Teste11 {
+    public static void main(String[] args) {
+        Conta c1 = new Conta("Tiago", 1000, 123);
+        Conta c2 = new Conta("Maria", 500, 321);
+    }
+}
